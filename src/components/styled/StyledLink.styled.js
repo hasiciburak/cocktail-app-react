f0522dfcc17199d403934fceebaa84e70ea80e1c
Burak Haşıcı <burakhasici@gmail.com>
2022-11-14 22:13:08 +0300

    Added 404 Page

---
+++ src/components/styled/StyledLink.styled.js
@@ -0,0 +1,10 @@
+import tw from "twin.macro";
+
+const StyledLink = tw.a`
+    text-sm
+    no-underline
+    hover:underline
+    cursor-pointer
+`;
+
+export default StyledLink;
--- src/pages/NotFound/index.js
+++ src/pages/NotFound/index.js
@@ -1,7 +1,35 @@
 import React from "react";
-
+import Header1 from "../../components/styled/Header1.styled";
+import { Player } from "@lottiefiles/react-lottie-player";
+import cocktailAnimation from "../../assets/animations/cocktail-mix.json";
+import Header3 from "../../components/styled/Header3.styled";
+import StyledLink from "../../components/styled/StyledLink.styled";
+import { Link } from "react-router-dom";
 const NotFound = () => {
-  return <div>404 Not Found!</div>;
+  return (
+    <div className="flex flex-col justify-center align-center text-center h-screen">
+      <Header1>404 - Not Found</Header1>
+      <Player
+        autoplay
+        loop
+        src={cocktailAnimation}
+        className="w-full aspect-[16/9]"
+      ></Player>
+      <Header3>Please Try Again Or Refresh Page</Header3>
+      <div className="flex flex-row gap-3 wrap justify-center mt-2">
+        <StyledLink
+          onClick={() => {
+            window.location.reload();
+          }}
+        >
+          Try Again
+        </StyledLink>
+        <StyledLink>
+          <Link to="/"> Return To Home</Link>
+        </StyledLink>
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
