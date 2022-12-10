fe2007b3232bce075ef046f0d018ed7a395a0a2f
Burak Haşıcı <burakhasici@gmail.com>
2022-12-10 23:14:53 +0300

    Cocktail Motion Added

--- src/pages/home/components/SloganArea.js
+++ src/pages/home/components/SloganArea.js
@@ -1,12 +1,26 @@
 import Header1 from "../../../components/styled/Header1.styled";
-
 import React from "react";
 import Description from "../../../components/styled/Description.styled";
-
+import redCocktail from "../../../assets/img/cocktial-red.png";
+import { motion } from "framer-motion";
 const SloganArea = () => {
   return (
     <div className="text-center">
-      <Header1>The Crazy Bartender</Header1>
+      <div className="flex flex-row justify-center items-center gap-10">
+        <Header1>The Crazy Bartender</Header1>
+        <motion.img
+          src={redCocktail}
+          alt=""
+          className=" w-[50px]"
+          animate={{
+            x: [300, 0],
+            rotate: [1200, 0],
+          }}
+          transition={{
+            duration: 1,
+          }}
+        />
+      </div>
       <Description>
         Crazy cocktails from your crazy bartender BH,in one place
       </Description>
